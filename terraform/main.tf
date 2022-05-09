4e495cba1add2539a6e3232c99e5ff8d7c18b6fb
llteodoro <lucasluisto@gmail.com>
2022-05-09 19:26:02 -0300

    C3 - Subindo Arquivos do Ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,30 +4,54 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
-  }
-
-  required_version = ">= 0.14.9"
+}
+ required_version = ">= 0.14.9"
 }
 
 provider "aws" {
   profile = "default"
-  region  = var.regiao_aws
+  region  = var.aws_region
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0c4f7023847b90238" #"ami-0aeb7c931a5a61206"
-  instance_type = var.instancia
-  key_name = var.chave
+resource "aws_instance" "Vm-Linux" {
+  count = 2
+  ami           = lookup(var.aws_ami-linux,var.aws_region) # us-east-1
+  instance_type = var.aws_instance
+  key_name = var.aws_key_pair
 
   tags = {
-    Name = "Terceira instância"
+    Name = "vm-linux-${count.index}"
+    Group = "Webserver-Linux"
   }
+  depends_on = [
+    aws_security_group.webserver-sg
+  ]
+  vpc_security_group_ids = ["${aws_security_group.webserver-sg.id}"] 
 }
 
-resource "aws_key_pair" "chave-SSH" {
-    key_name = var.chave
-    public_key = file("${var.chave}.pub")
+resource "aws_instance" "Vm-Windows" {
+  count = 2
+  ami           = lookup(var.aws_ami-windows,var.aws_region) # us-east-1
+  instance_type = var.aws_instance
+  key_name = var.aws_key_pair
   
+
+  tags = {
+    Name = "vm-windows-${count.index}"
+    Group = "Webserver-Windows"
+  }
+  depends_on = [
+    aws_security_group.webserver-sg
+  ]
+  vpc_security_group_ids = ["${aws_security_group.webserver-sg.id}"] 
 }
-output "IP_publico" {
-  value = aws_instance.app_server.public_ip
+
+
+resource "aws_key_pair" "chave-SSH" {
+    key_name = var.aws_key_pair
+    public_key = file("${var.aws_key_pair}.pub")
+
+
+}
+
+
